27cfbc142ea17de10e71623a63aeecf1801a6c61
AlpenFlizzer <47642539+AlpenFlizzer@users.noreply.github.com>
2021-09-24 20:03:37 +0200

    Update dependencies.sh

--- janus/dependencies.sh
+++ janus/dependencies.sh
@@ -2,4 +2,5 @@
 apt-get install -y libmicrohttpd-dev libjansson-dev libnice-dev\
 	libssl-dev libsrtp2-dev libsofia-sip-ua-dev libglib2.0-dev \
 	libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
-	libconfig-dev pkg-config gengetopt libtool automake tzdata
+	libconfig-dev pkg-config gengetopt libtool automake tzdata \
+	avahi-daemon avahi-discover libnss-mdns
